1d8b05441d407db5d549c06dd95c28f791e117de
AlexDom513 <57962269+AlexDom513@users.noreply.github.com>
2022-01-11 17:43:41 -0600

    Added SLP System Code and updated TVOC sensor code

--- SLP/TVOCsensor/tvoc/tvoc.ino
+++ SLP/TVOCsensor/tvoc/tvoc.ino
@@ -1,31 +1,48 @@
-#include <Adafruit_CCS811.h>
+#include <Wire.h>
 
-Adafruit_CCS811 ccs;
+#include "SparkFunCCS811.h" //Click here to get the library: http://librarymanager/All#SparkFun_CCS811
 
-void setup() {
-  serial.begin(9600);
-  ccs.begin();
-  //calibrate temperature sensor
-  while(!ccs.available());
-  float temp = ccs.calculateTemperature();
-  ccs.setTempOffset(temp - 25.0);
+#define CCS811_ADDR 0x5B //Default I2C Address
+//#define CCS811_ADDR 0x5A //Alternate I2C Address
+
+CCS811 mySensor(CCS811_ADDR);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("CCS811 Basic Example");
+
+  Wire.begin(); //Inialize I2C Hardware
+
+  if (mySensor.begin() == false)
+  {
+    Serial.print("CCS811 error. Please check wiring. Freezing...");
+    while (1)
+      ;
+  }
 }
 
-void loop(){
-  if(ccs.available()){
-    float temp = ccs.calculateTemperature();
+void loop()
+{
+  //Check to see if data is ready with .dataAvailable()
+  if (mySensor.dataAvailable())
+  {
+    //If so, have the sensor read and calculate the results.
+    //Get them later
+    mySensor.readAlgorithmResults();
 
-    if(!ccs.readData()){
-      int co2 = ccs.geteCO2();
-      int tvoc = ccs.getTVOC();
-      serial.println(tvoc);
-      serial.println(co2);
-      delay(3000);    
-    }
-
-    else{
-      serial.println("ERROR");
-      while(1);
-    }
+    Serial.print("CO2[");
+    //Returns calculated CO2 reading
+    Serial.print(mySensor.getCO2());
+    Serial.print("] tVOC[");
+    //Returns calculated TVOC reading
+    Serial.print(mySensor.getTVOC());
+    Serial.print("] millis[");
+    //Display the time since program start
+    Serial.print(millis());
+    Serial.print("]");
+    Serial.println();
   }
+
+  delay(10); //Don't spam the I2C bus
 }
